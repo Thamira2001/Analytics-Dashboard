41fcbf3c676d4d6079702e8b10b459e497c84b22
Thamira Mishal Randeniya <130529871+Thamira2001@users.noreply.github.com>
2024-08-05 23:53:44 +0000

    second commit made changes to created and gender in api calls using serializers

--- analytics/backend/customers/api/serializers.py
+++ analytics/backend/customers/api/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
 from ..models import Customer
+from ..choices import GENDER_CHOICES
+
+class GenderChoiceFieldSerializer(serializers.Field):
+    def to_representation(self, obj):
+        return dict(GENDER_CHOICES)[obj]
+
+    def to_internal_value(self, data):
+        return data
 
 class CustomerSerializer(serializers.ModelSerializer):
+    gender= GenderChoiceFieldSerializer()
+    created = serializers.SerializerMethodField()
     class Meta:
         model = Customer
         fields = (
@@ -12,3 +22,6 @@ class CustomerSerializer(serializers.ModelSerializer):
             'favorite_number',
             'created'
         )
+
+    def get_created(self, obj):
+        return obj.created.strftime('%Y-%m-%d %H:%M:%S')
