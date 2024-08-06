5290f6396d942833f2d8b16c0de373c958dd96f0
Thamira Mishal Randeniya <130529871+Thamira2001@users.noreply.github.com>
2024-08-06 00:18:17 +0000

    changes to app.py

--- analytics/app.py
+++ analytics/app.py
@@ -1,6 +1,15 @@
 import streamlit as st 
-import matplotlib
+import matplotlib.pylot as plt
 
 st.title("Analytics Dashboard")
 st.write("v.0.0.1")
 
+#Test Chart
+categories = ['A', 'B', 'C', 'D']
+values = np.random.randint(10,100, size=(4,))
+
+fig, ax = plt.subplot()
+ax.bar(categories, values, color='blue')
+ax.set_xlabel('categories')
+ax.set_ylable('values')
+ax.set_title
