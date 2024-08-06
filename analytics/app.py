3673170a74b3301bd1affee01f005ae89c2c3760
Thamira Mishal Randeniya <130529871+Thamira2001@users.noreply.github.com>
2024-08-06 00:29:41 +0000

    first bar chart in streamlit

--- analytics/app.py
+++ analytics/app.py
@@ -1,15 +1,17 @@
-import streamlit as st 
-import matplotlib.pylot as plt
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
 
 st.title("Analytics Dashboard")
 st.write("v.0.0.1")
 
 #Test Chart
-categories = ['A', 'B', 'C', 'D']
-values = np.random.randint(10,100, size=(4,))
+# categories = ['A', 'B', 'C', 'D']
+# values = np.random.randint(10,100, size=(4,))
 
-fig, ax = plt.subplot()
-ax.bar(categories, values, color='blue')
-ax.set_xlabel('categories')
-ax.set_ylable('values')
-ax.set_title
+# fig, ax = plt.subplot()
+# ax.bar(categories, values, color='blue')
+# ax.set_xlabel('categories')
+# ax.set_ylabel('values')
+# ax.set_title('First bar Chart')
+# st.pyplot(fig)
